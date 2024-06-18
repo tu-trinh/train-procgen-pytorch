dedb44949975ae35ab779dfe382c7cd9f996d565
Tu Trinh <tutrinh@berkeley.edu>
2024-06-17 18:59:49 -0700

    receiving help experiments

--- ask_for_help_experiments.sh
+++ ask_for_help_experiments.sh
@@ -3,6 +3,9 @@
 seed=8888
 risk_values=(10 20 30 40 50 60 70 80 90)
 
+index=$1
+gpu_device=$2
+
 # SIMPLE TRAIN ENVIRONMENT
 # python3 render.py \
 #     --exp_name eval_train_og \
@@ -58,25 +61,24 @@ metrics=(
     "sampled_l"
     "ent"
 )
-for i in "${!names[@]}"; do
-    for risk in "${risk_values[@]}"; do
-        python3 render.py \
-            --exp_name "${names[i]}_risk_${risk}" \
-            --env_name coinrun_aisc \
-            --distribution_mode hard \
-            --param_name hard-plus \
-            --model_file logs/train/coinrun/og_actions/2024-05-25__19-35-47__seed_8888/model_200015872.pth \
-            --percentile_dir logs/procgen/coinrun/get_percentiles_og/RENDER_seed_8888_06-10-2024_13-45-01 \
-            --select_mode sample \
-            --ood_metric ${metrics[i]} \
-            --risk ${risk} \
-            --expert_model_file logs/train/coinrun_aisc/expert/2024-06-10__08-20-01__seed_8888/model_200015872.pth \
-            --expert_cost 5 \
-            --switching_cost 5 \
-            --quant_eval \
-            --seed ${seed} \
-            --save_run
-    done
+for risk in "${risk_values[@]}"; do
+    python3 render.py \
+        --exp_name "${names[index]}_risk_${risk}" \
+        --env_name coinrun_aisc \
+        --distribution_mode hard \
+        --param_name hard-plus \
+        --model_file /nas/ucb/tutrinh/train-procgen-pytorch/logs/using/coinrun/model_200015872.pth \
+        --percentile_dir /nas/ucb/tutrinh/train-procgen-pytorch/logs/using/coinrun \
+        --select_mode sample \
+        --ood_metric ${metrics[index]} \
+        --risk ${risk} \
+        --expert_model_file /nas/ucb/tutrinh/train-procgen-pytorch/logs/using/coinrun_aisc/model_200015872.pth \
+        --expert_cost 2 \
+        --switching_cost 2 \
+        --quant_eval \
+        --seed ${seed} \
+        --gpu_device ${gpu_device} \
+        --save_run
 done
 
 # PER-ACTION METRICS, TEST ENVIRONMENT
