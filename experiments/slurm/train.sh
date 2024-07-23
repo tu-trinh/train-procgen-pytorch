8a589296290b68a7cc9ff0b148247728dfc7738f
tu-trinh <tutrinh@berkeley.edu>
2024-07-23 11:46:43 -0700

    edits

--- experiments/slurm/train.sh
+++ experiments/slurm/train.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=40gb
 #SBATCH --gres=gpu:1
 #SBATCH --wait-all-nodes=1
-#SBATCH --job-name=save_coinrun
+#SBATCH --job-name=save_heist_aisc_many_chests
 #SBATCH --output=experiments/slurm/%x.out
 #SBATCH --time=72:00:00
 #SBATCH --qos scavenger
@@ -19,12 +19,12 @@ cd /nas/ucb/tutrinh/train-procgen-pytorch
 export PYTHONPATH="$PYTHONPATH:$PWD"
 
 python3 train.py \
-        --env_name coinrun \
+        --env_name heist_aisc_many_chests \
         --exp_name save_obs_throughout \
         --num_levels 100000 \
         --distribution_mode hard \
         --param_name hard-500 \
-        --num_timesteps 6500000 \
+        --num_timesteps 200000000 \
         --save_timesteps 390625 781250 1562500 3125000 6250000 10000000 12500000 20000000 25000000 30000000 40000000 50000000 60000000 70000000 80000000 90000000 100000000 110000000 120000000 130000000 140000000 150000000 160000000 170000000 180000000 190000000 200000000 \
         --seed 8888 \
         --use_wandb \
